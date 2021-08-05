90a1e805e7a4b27746a631b5eba427b0ebde4c07
Mateusz Grzybek <mateusz.grzybek@tietoevry.com>
2021-08-05 10:10:27 +0200

    Add axios. Prepare script for handling backend requests.

---
+++ src/components/mongo-express-script.js
@@ -0,0 +1,58 @@
+import axios from "axios";
+
+export const createNewEntry = async item => {
+    let data = {
+        pilotName: item.pilotName, // authorized user should be used here in future prod build
+        depICAO: item.depICAO,
+        arrICAO: item.arrICAO,
+        depTimeZulu: JSON.stringify(item.depTimeZulu),
+        arrTimeZulu: JSON.stringify(item.arrTimeZulu),
+        flightTime: JSON.stringify(item.flightTime),
+    };
+    let request = {
+        url: "http://localhost:3000/api/logbook", // domain should be used here in prod build
+        method: "post",
+        headers: {
+            "Content-type": "application/json",
+        },
+        data: JSON.stringify(data),
+    };
+
+    const response = await axios(request);
+    return response;
+};
+
+export const updateEntry = async item => {
+    let data = {
+        pilotName: item.pilotName, // authorized user should be used here in future prod build
+        depICAO: item.depICAO,
+        arrICAO: item.arrICAO,
+        depTimeZulu: JSON.stringify(item.depTimeZulu),
+        arrTimeZulu: JSON.stringify(item.arrTimeZulu),
+        flightTime: JSON.stringify(item.flightTime),
+    };
+    let request = {
+        url: "http://localhost:3000/api/logbook/" + item._id, // domain should be used here in prod build
+        method: "put",
+        headers: {
+            "Content-type": "application/json",
+        },
+        data: JSON.stringify(data),
+    };
+
+    const response = await axios(request);
+    return response;
+};
+
+export const getAllEntries = async () => {
+    let request = {
+        url: "http://localhost:3000/api/logbook", // domain should be used here in prod build
+        method: "get",
+        headers: {
+            "Content-type": "application/json",
+        },
+    };
+
+    const response = await axios(request);
+    return response;
+};
