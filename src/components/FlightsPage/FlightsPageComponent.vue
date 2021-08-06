e80bfa4d11fcb9091848e6102e9b2656b51b018b
Mateusz Grzybek <mateusz.grzybek@tietoevry.com>
2021-08-06 14:34:22 +0200

    Add feature to delete log entries. Add vue.config.js

--- src/components/FlightsPage/FlightsPageComponent.vue
+++ src/components/FlightsPage/FlightsPageComponent.vue
@@ -15,12 +15,23 @@
                     <span>Log a new flight</span>
                 </router-link>
                 <div v-for="logbookEntry in logbookEntries" :key="logbookEntry._id" class="mb-6">
-                    <p>Pilot's name: {{ logbookEntry.pilotName }}</p>
-                    <p>Departure airport ICAO: {{ logbookEntry.depICAO }}</p>
-                    <p>Arrival airport ICAO: {{ logbookEntry.arrICAO }}</p>
-                    <p>Departure time: {{ logbookEntry.depTimeZulu }}</p>
-                    <p>Arrival time: {{ logbookEntry.arrTimeZulu }}</p>
-                    <p>Flight time: {{ logbookEntry.flightTime }}</p>
+                    <div class="columns is-align-items-center">
+                        <div class="column is-10 mr-3">
+                            <p>Pilot's name: {{ logbookEntry.pilotName }}</p>
+                            <p>Departure airport ICAO: {{ logbookEntry.depICAO }}</p>
+                            <p>Arrival airport ICAO: {{ logbookEntry.arrICAO }}</p>
+                            <p>Departure time: {{ logbookEntry.depTimeZulu }}</p>
+                            <p>Arrival time: {{ logbookEntry.arrTimeZulu }}</p>
+                            <p>Flight time: {{ logbookEntry.flightTime }}</p>
+                        </div>
+                        <div class="column is-2">
+                            <button type="button" class="button is-white is-outlined" v-on:click="deleteEntry(logbookEntry)">
+                                <span class="icon">
+                                    <i class="fa fa-trash"></i>
+                                </span>
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -28,17 +39,29 @@
 </template>
 
 <script>
-import { getAllEntries } from "../mongo-express-script";
+import { getAllEntries, deleteEntry } from "../mongo-express-script";
 
 export default {
     name: "FlightsPage",
     data() {
         return {
-            logbookEntries: null,
+            logbookEntries: [],
         };
     },
     mounted() {
         getAllEntries().then(response => (this.logbookEntries = response.data));
+    },
+    activated() {
+        getAllEntries().then(response => (this.logbookEntries = response.data));
+    },
+    methods: {
+        deleteEntry(entry) {
+            deleteEntry(entry).then(response => {
+                if (response.status === 200) {
+                    this.logbookEntries.splice(this.logbookEntries.indexOf(entry), 1);
+                }
+            });
+        },
     },
 };
 </script>
