fca3229a90d7b56d8faa813428b110520fb77384
Mateusz Grzybek <mateusz.grzybek06@gmail.com>
2021-08-20 15:59:34 +0200

    Increase encryption salt rounds

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -40,7 +40,7 @@ const userSchema = new Schema(
 userSchema.pre("save", async function(next) {
     const user = this;
     if (user.isModified("password")) {
-        user.password = await bcrypt.hash(user.password, 8);
+        user.password = await bcrypt.hash(user.password, 12);
     }
     next();
 });
