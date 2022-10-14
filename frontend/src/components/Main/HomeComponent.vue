cc54e98d6b44cc9feb3af83263037e025b5b5d03
Mateusz Grzybek <mateusz.grzybek@codema.pl>
2022-10-14 15:10:01 +0200

    Adjust home component typography

--- frontend/src/components/Main/HomeComponent.vue
+++ frontend/src/components/Main/HomeComponent.vue
@@ -1,12 +1,15 @@
 <template>
     <div class="container">
-        <section class="hero is-fullheight is-justify-content-center">
-            <div class="has-text-white">
+        <section class="hero is-fullheight is-justify-content-center is-align-items-end">
+            <div class="has-text-white has-text-right">
                 <template v-if="!isUserLoggedIn">
                     <div class="is-size-2">Welcome to your personal logbook</div>
                     <div class="is-size-4">Please log in or register to use the full featureset of the application</div>
                 </template>
-                <template v-else>Welcome to your personal logbook, {{ firstName }}</template>
+                <template v-else>
+                    <div class="is-size-2">Welcome to your personal logbook, {{ firstName }}</div>
+                    <div class="is-size-4">Use the side menu to choose your next action</div>
+                </template>
             </div>
         </section>
     </div>
